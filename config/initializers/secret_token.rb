d6e16f3489b8b850c66420ce5af413ef9180a83f
njohnson <cerebrus619@gmail.com>
2013-11-03 09:51:06 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CesiumProject::Application.config.secret_token = '95182ab5a9050937d6465be0318109a6f673d6323b2e17ebe3a4cf0ac11e13f722c9e74d937f075c356b6c106ecddb3f62d2c2750bd84dbcc0b7547e9b53d40b'
